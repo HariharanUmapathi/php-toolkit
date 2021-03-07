9a8dc883055eba8c06b42c4cc33fe3c429cd3ceb
Hariharan U <smarthariharan28@gmail.com>
2021-03-07 23:37:55 +0530

    Add File:index.php, dbms.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?PHP
+include "dbms.php";
+$db=new dbAdapter("localhost","root","","lib_mgmt");
+$_GET["json"]=false;
+$db->query("select * from collection limit 0,10");
+if($_GET["json"]==true){
+	header("Content-type:application/json");
+	echo json_encode($db->recordsArray());	
+	exit(0);
+}
+else{
+	
+	tableTag($db->recordsArray(false));
+	
+}
+function tableTag($array){
+	echo "\n<table>\n";
+	foreach($array as $tr){
+	echo "	<tr>\n";
+		foreach($tr as $td1=>$td2)
+		echo "		<td>".$td1."</td><td>".$td2."</td>\n";
+	echo "	<tr>\n";
+	}
+	echo "</table>\n";
+	
+
+	
+}
+?>
